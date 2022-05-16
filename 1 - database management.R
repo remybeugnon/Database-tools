c5a7d21ecf5d5152d48a67907bd70cbe6d3d7f2c
remybeugnon <39779230+remybeugnon@users.noreply.github.com>
2022-05-16 08:34:10 +0200

    HIP

---
+++ 1 - database management.R
@@ -0,0 +1,9 @@
+#### > 1. Management functions ####
+
+#### >> 1.1. Add missing data from table ####
+
+#### >> 1.2. Compare to table and update ####
+
+#### > 2. Visualization ####
+
+#### >> 2.1. Plot database structure ####
---
+++ 2 - database use.R
@@ -0,0 +1,6 @@
+#### > 1. Data summarization ####
+
+#### >> 1.1. Summarizing of the tables ####
+
+#### > 2. Data extraction ####
+
